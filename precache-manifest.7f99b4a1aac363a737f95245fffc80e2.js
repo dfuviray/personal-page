ac58213efdac8136d392e1ab7b307a79d6cb972a
dfuviray <deo.uviray@gmail.com>
2019-12-18 23:11:08 +0800

    Updates

---
+++ precache-manifest.7f99b4a1aac363a737f95245fffc80e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c386f5307cb4d72f783a4b0cfe26e9",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "fa9ca2e21657f2a773ec",
+    "url": "/personal-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c21dfe166688b33c0ec9",
+    "url": "/personal-page/static/js/2.562c2972.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/personal-page/static/js/2.562c2972.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa9ca2e21657f2a773ec",
+    "url": "/personal-page/static/js/main.2edf098a.chunk.js"
+  },
+  {
+    "revision": "2f94c58f3bc2ba3928d5",
+    "url": "/personal-page/static/js/runtime-main.ae9ec997.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
