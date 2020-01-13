c4cc62d796440572b6203d29fa5eca345730d06f
dfuviray <deo.uviray@gmail.com>
2020-01-13 22:25:39 +0800

    Updates

---
+++ precache-manifest.30f40fcb8e55ca536b8c97127d341dd2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283631bad8659d5bac5e871f88b016bf",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "c11c6a00aade114d73cc",
+    "url": "/personal-page/static/css/2.3b3c448a.chunk.css"
+  },
+  {
+    "revision": "aef611f88a2cf591e5f9",
+    "url": "/personal-page/static/css/main.d9bfe596.chunk.css"
+  },
+  {
+    "revision": "c11c6a00aade114d73cc",
+    "url": "/personal-page/static/js/2.c5b5fb0c.chunk.js"
+  },
+  {
+    "revision": "f2d66193655f6a02c3f07c2a2c3aea10",
+    "url": "/personal-page/static/js/2.c5b5fb0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aef611f88a2cf591e5f9",
+    "url": "/personal-page/static/js/main.b7bbeb85.chunk.js"
+  },
+  {
+    "revision": "2f94c58f3bc2ba3928d5",
+    "url": "/personal-page/static/js/runtime-main.ae9ec997.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/personal-page/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/personal-page/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/personal-page/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/personal-page/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/personal-page/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/personal-page/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/personal-page/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/personal-page/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/personal-page/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bb4d045a18534e30c6ddbb725d9034e0",
+    "url": "/personal-page/static/media/logo.bb4d045a.svg"
+  },
+  {
+    "revision": "6b58b961e0f72a44c4c0b82aefa6737a",
+    "url": "/personal-page/static/media/profile.6b58b961.png"
+  }
+]);
